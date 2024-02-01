c23ad448f707aac8448feede6eb15851e670d1ed
Alexandra Bunty <buntyalexandra@gmail.com>
2024-02-01 15:30:53 -0500

    initial testing of GitHub user search API

--- app/js/search-result.js
+++ app/js/search-result.js
@@ -1,5 +1,26 @@
 // search feature
-// select the search input by class name
-const searchInput = document.querySelector(".searchInput");
+// select the search input by id
+const searchInput = document.getElementById("searchInput");
+// select seach button by id
+const searchButton = document.getElementById("searchButton");
+// declare the input value
+const inputValue = searchInput.value;
+// function to get input value and print it to the console
+function getInputVal() {
+  console.log("inputValue:", inputValue);
+}
+// add on click event listener to search button
+searchButton.addEventListener("click", getInputVal);
+
+function fetchUser(user) {
+  fetch(`https://api.github.com/users/${searchInput}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log("data:", data);
+    })
+    .catch((error) => console.log(error));
+}
 
 // update search result card
